344db59fc58f9f4ce9e4df3906fea1c35f05631e
Tutinichic <da295615@gmail.com>
2023-04-04 00:51:41 +0300

    added matrix

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,18 @@
+import numpy
 from fastapi import APIRouter
 
 router = APIRouter()
 
-
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10).tolist()
+    matrix_b = numpy.random.rand(10, 10).tolist()
+    product = numpy.dot(matrix_a, matrix_b).tolist()
+    
+    result = {
+        "matrix_a": matrix_a, 
+        "matrix_b": matrix_b, 
+        "product": product
+    }
+    
+    return result
